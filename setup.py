2c9847a86949ba45b71cf3905d32379247d1fde0
pl <0x00.pl@gmail.com>
2017-02-23 18:39:17 +0800

    add setup script

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
 
 setup(
+    name="pl-jsonrpc",
+    version="0.1",
+    packages=['pl-jsonrpc'],
+    install_requires=[],
+    zip_safe=True,
+    test_suite="tests"
 )
